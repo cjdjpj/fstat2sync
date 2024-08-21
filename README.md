cfdeea8a2bee17c55e4af9eb1c34dbf498b02337
Cj Jiang <cjdjpj@hotmail.com>
2024-08-21 22:49:36 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# fstat2sync
+
+Simple utility to convert [FSTAT files](https://doi.org/10.1111/j.1471-8286.2004.00828.x) to sync ([synchronized pileup files](https://doi.org/10.1093%2Fbioinformatics%2Fbtr589)).
+No chromosomal/genetic structure data (marked NA).
---
+++ src/main.rs
@@ -0,0 +1,111 @@
+#![allow(warnings)]
+use clap::Parser;
+use std::error::Error;
+use std::io::{Write, BufRead, BufReader};
+use std::fs::File;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args{
+    ///Input FSTAT (Goudet 1995) file
+    #[clap(short, long)]
+    fname: String,
+
+    ///Output synchronized pileup file
+    #[clap(short, long)]
+    output: String,
+}
+
+#[derive(Debug)]
+struct Fstat{
+    patches: u32,
+    num_loci: u32,
+    num_alleles: u32,
+    allele_digits: u32,
+    allele_counts: Vec<Vec<(u32, u32, u32, u32)>>
+}
+
+impl Fstat{
+    fn read_fstat(fname: &String) -> Result<Self, Box<dyn Error>>{
+        let fstat_file = File::open(fname)?;
+        let mut fstat_reader = BufReader::new(fstat_file);
+
+        let mut metadata = String::new();
+        fstat_reader.read_line(&mut metadata)?;
+
+        let metadata: [&str; 4] = metadata.split_whitespace()
+            .collect::<Vec<&str>>()
+            .try_into()
+            .unwrap();
+
+        let patches: u32 = metadata[0].parse().unwrap();
+        let num_loci: u32 = metadata[1].parse().unwrap();
+        let num_alleles: u32 = metadata[2].parse().unwrap();
+        let allele_digits: u32 = metadata[3].parse().unwrap();
+
+        // skip locus names
+        for _ in 0..num_loci{
+            let _ = fstat_reader.read_line(&mut String::new())?;
+        }
+
+        let mut allele_counts: Vec<Vec<(u32, u32, u32, u32)>> = vec![vec![(0,0,0,0); patches as usize]; num_loci as usize];
+
+        // read genotypes
+        let mut genotype = String::new();
+        while fstat_reader.read_line(&mut genotype)? > 0 {
+            let mut reads: Vec<&str> = genotype.split_whitespace().collect();
+            let patch = reads.remove(0).parse::<usize>()? - 1;
+            for (l, read) in reads.iter().enumerate(){
+                let allele1 = read[0..allele_digits as usize].parse::<u32>().unwrap();
+                let allele2 = read[allele_digits as usize..read.len()].parse::<u32>().unwrap();
+                match allele1{
+                    1 => allele_counts[l][patch].0 += 1,
+                    2 => allele_counts[l][patch].1 += 1,
+                    3 => allele_counts[l][patch].2 += 1,
+                    4 => allele_counts[l][patch].3 += 1,
+                    _ => panic!("Invalid allelic value"),
+                }
+                match allele2{
+                    1 => allele_counts[l][patch].0 += 1,
+                    2 => allele_counts[l][patch].1 += 1,
+                    3 => allele_counts[l][patch].2 += 1,
+                    4 => allele_counts[l][patch].3 += 1,
+                    _ => panic!("Invalid allelic value"),
+                }
+            }
+            genotype.clear(); 
+        }
+
+        Ok(Fstat{
+            patches,
+            num_loci,
+            num_alleles,
+            allele_digits,
+            allele_counts,
+        })
+    }
+
+    fn write2sync(&self, output: &String) -> Result<(), Box<dyn Error>>{
+        let mut sync_file = File::create(output)?;
+        let mut header = "chr\tpos\tref".to_owned();
+        for i in 0..self.patches{
+            header = format!("{header}\tpatch{i}")
+        }
+        writeln!(sync_file, "#{header}")?;
+        for (locus_index, locus) in self.allele_counts.iter().enumerate(){
+            let mut locus_string = format!("NA\t{locus_index}\tT");
+            for pool in locus{
+                locus_string = format!("{locus_string}\t{}:{}:{}:{}:0:0", pool.0, pool.1, pool.2, pool.3)
+            }
+            writeln!(sync_file, "{}", locus_string)?;
+        }
+        Ok(())
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>>{
+    let args = Args::parse();
+    let fstat = Fstat::read_fstat(&args.fname)?;
+    let _ = fstat.write2sync(&args.output);
+    Ok(())
+}
